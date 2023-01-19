9dac26b44d72c1ebaa47561bea63f473e7e86d26
James Ward <jimmywardmichael@gmail.com>
2023-01-19 11:47:46 -0600

    still have errors

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,11 @@
-USE
-codeup_test_db;
+USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums
-(
-    id INT PRIMARY KEY UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist CHAR,
     name CHAR,
     release_date INT,
     sales FLOAT,
     genre VARCHAR,
+    PRIMARY KEY (id)
 );

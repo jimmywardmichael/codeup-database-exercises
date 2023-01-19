ab2728fef267c1dc75f564c8598ec9c98966b6d7
James Ward <jimmywardmichael@gmail.com>
2023-01-19 11:57:00 -0600

    c

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,11 @@ codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums
 (
-    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist       CHAR    NOT NULL,
-    record_name         CHAR    NOT NULL,
-    release_date INT     NOT NULL,
-    sales        FLOAT   NOT NULL,
-    genre        VARCHAR NOT NULL,
-        PRIMARY KEY (id)
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist CHAR NOT NULL,
+    record_name CHAR NOT NULL,
+    release_date INT NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR NOT NULL,
+    PRIMARY KEY (id)
 );

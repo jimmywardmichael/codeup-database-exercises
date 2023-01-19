a24c403635c8db8962e0814d47f37ff1a24061b9
James Ward <jimmywardmichael@gmail.com>
2023-01-19 11:53:06 -0600

    added record to name

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,13 @@
-USE codeup_test_db;
+USE
+codeup_test_db;
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums(
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist CHAR,
-    name CHAR,
-    release_date INT,
-    sales FLOAT,
-    genre VARCHAR,
-    PRIMARY KEY (id)
+CREATE TABLE albums
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist       CHAR    NOT NULL,
+    record_name         CHAR    NOT NULL,
+    release_date INT     NOT NULL,
+    sales        FLOAT   NOT NULL,
+    genre        VARCHAR NOT NULL,
+        PRIMARY KEY (id)
 );

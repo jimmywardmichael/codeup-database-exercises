966083d7f3cd932b9ab687fbf67a8fd82469fafb
James Ward <jimmywardmichael@gmail.com>
2023-01-23 12:11:40 -0600

    changes made

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,6 +1,6 @@
 USE employees;
 
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name OR ('Irena', 'Vidya', 'Maya');
 
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 
@@ -15,3 +15,11 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+
+SELECT * FROM employees WHERE gender LIKE 'M' AND gender NOT LIKE 'F';
+
+SELECT * FROM employees WHERE last_name LIKE  'E%';
+
+
+
+SELECT  * FROM employees WHERE last_name LIKE 'q' NOT LIKE 'qu';

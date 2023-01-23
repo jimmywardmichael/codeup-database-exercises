c7dd068053a709c756a6a5c87eb723080348fc92
James Ward <jimmywardmichael@gmail.com>
2023-01-23 13:57:47 -0600

    started limit exer

---
+++ limit_exercises.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT title FROM titles;
+
+SELECT last_name
+FROM employees
+ORDER BY last_name DESC;
+
+SELECT emp_no, first_name, last_name
+FROM salaries
+WHERE salary
+    LIMIT 5;
+

b8d98b5d501d12e53b5d903dbc0d9ccf004407fc
James Ward <jimmywardmichael@gmail.com>
2023-01-25 11:01:41 -0600

    changes

--- join_exercises.sql
+++ join_exercises.sql
@@ -18,4 +18,25 @@ FROM employees as e
 where dm.to_date = '9999-01-01' AND e.gender = 'F' ORDER BY d.dept_name;
 
 #3
+SELECT title, COUNT(title) AS total
+FROM titles AS t
+JOIN employees e on e.emp_no = t.emp_no
+JOIN dept_emp d on e.emp_no = d.emp_no
+WHERE d.dept_no = 'd009'
+AND t.to_date = '9999-01-01'
+  AND d.to_date = '9999-01-01'
+GROUP BY title;
 
+#4
+SELECT d.dept_name AS 'Department Name ', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary
+FROM salaries as s
+JOIN employees e on e.emp_no = s.emp_no
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+where dm.to_date = '9999-01-01'
+AND s.to_date = '9999-01-01'
+ORDER BY d.dept_name;
+
+#5

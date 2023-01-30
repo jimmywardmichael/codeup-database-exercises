c11ea9166a10fbe51473e9a55d652e38a578dfe2
James Ward <jimmywardmichael@gmail.com>
2023-01-30 08:41:27 -0600

    finished exer

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,6 +1,14 @@
 USE employees;
 
-SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) as full_name,
- birth_date   as DOB
+SELECT CONCAT(last_name, ', ', first_name) AS full_name
 FROM employees
-ORDER BY emp_no LIMIT 10;
+LIMIT 10;
+
+SELECT CONCAT(last_name, ', ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+LIMIT 10;
+
+SELECT CONCAT(emp_no, ' - ', last_name, ', ', first_name) AS full_name, birth_date AS DOB
+FROM employees
+ORDER BY emp_no ASC
+LIMIT 10;
